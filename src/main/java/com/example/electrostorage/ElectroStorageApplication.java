f9f177fc5973e5155d1eb2145c7b84d5d1aadcce
Rasmus Haslund <haslund94@icloud.com>
2026-08-20 10:01:19 +0200

    Settup project

---
+++ src/main/java/com/example/electrostorage/ElectroStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.electrostorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/electrostorage/ElectroStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electrostorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
